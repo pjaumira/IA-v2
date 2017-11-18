d10ec931602dcdc71b917128bdb0805626867ea6
Portatil <togirainoroka@gmail.com>
2017-11-18 21:48:45 +0100

    Neteaja i preparacio altres exercicis

--- SDL_Pathfinding/src/main.cpp
+++ SDL_Pathfinding/src/main.cpp
@@ -2,11 +2,18 @@
 #include <SDL.h>
 #include <SDL_image.h>
 
+//Ecercici 1
 #include "SDL_SimpleApp.h"
 #include "ScenePathFinding.h"
 #include "SceneDjikstra.h"
 #include "SceneGreedyBFS.h"
 #include "SceneAEstrella.h"
+
+//Exercici 2
+#include  "SceneRandomPoints.h"
+
+//Exercici 3
+#include "SceneAdversaris.h"
 
 #define FRAMES_PER_SEC 30
 
@@ -62,6 +69,18 @@ int main(int argc, char ** argv)
 			{
 				delete(curr_scene);
 				//curr_scene = new SceneAEstrella;
+				app->setWindowTitle(curr_scene->getTitle());
+			}
+			if (event.key.keysym.scancode == SDL_SCANCODE_5)
+			{
+				delete(curr_scene);
+				//curr_scene = new SceneRandomPoints;
+				app->setWindowTitle(curr_scene->getTitle());
+			}
+			if (event.key.keysym.scancode == SDL_SCANCODE_6)
+			{
+				delete(curr_scene);
+				//curr_scene = new SceneAdversaris;
 				app->setWindowTitle(curr_scene->getTitle());
 			}
 			if ((event.key.keysym.scancode == SDL_SCANCODE_Q) || (event.key.keysym.scancode == SDL_SCANCODE_ESCAPE))
